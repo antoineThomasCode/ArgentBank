fe69741bca5fae86a483290ea9fbc52301783fac
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-06-30 16:31:01 +0200

    divid homepage with components

--- App/src/pages/homePage/Home.jsx
+++ App/src/pages/homePage/Home.jsx
@@ -1,30 +1,14 @@
 import React from "react";
-import bannerImage from "../../assets/images/bank-tree.jpeg";
-import "../homePage/Home.scss"
 
-import InfoCards from "./InfoCard";
+import "../homePage/Home.scss"
+import Banner from "../../components/Homepage/Banner";
+import InfoCards from "../../components/Homepage/InfoCard";
 import infoCardsArray from "../../data/infoCardsArray";
 
 function Home () {
     return (
         <main id="homePage">
-        <section className="banner-container">
-            <figure>
-                <img src={bannerImage} alt="Plante qui pousse dans une verre rempli de pièces de monnaie" />
-            </figure>
-            <div className="slogan-container">
-                <h2>
-                    No fees. <br />
-                    No minimum deposit. <br />
-                    High interest rates.
-                </h2>
-                <p>
-                    Open a savings account with <br />
-                    Argent Bank today!
-                </p>
-
-            </div>
-        </section>
+        <Banner />
         <section className="infosCard-container">
             <InfoCards cardsArray={infoCardsArray} />
         </section>
